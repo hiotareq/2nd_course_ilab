e6c38ccaf6a11815534c331d189be7c24f6a879b
hiotareq <62850531+hiotareq@users.noreply.github.com>
2020-09-29 22:19:59 +0300

    Update triangle.cpp

--- triangles/triangle.cpp
+++ triangles/triangle.cpp
@@ -1,9 +1,7 @@
-#include <iostream>
 #include "triangle.h"
-#include <cmath>
 
 //it is the maaaaaain function of this problem
-bool triangle_geometry::triangle_handler::is_intersect(const triangle &tr1, triangle &tr2) const {
+bool triangle_geometry::triangle_handler::is_intersect(const triangle &tr1, const triangle &tr2) const {
     if ( tr1.is_degenerate() ){
         /*
          * удаление вырожденного треугольника из базы данных
@@ -14,23 +12,35 @@ bool triangle_geometry::triangle_handler::is_intersect(const triangle &tr1, tria
          * удаление вырожденного треугольника из базы данных
          */
     }
-    Plane plane_1 = cal_plane(tr1 );
+    Plane plane_1 = Plane( tr1);
     int sign1, sign2, sign3;
-    sign1 = triangle_handler::sign_of_dist(plane_1, tr2.get_vertice(1));
-    sign2 = triangle_handler::sign_of_dist(plane_1, tr2.get_vertice(2));
-    sign3 = triangle_handler::sign_of_dist(plane_1, tr2.get_vertice(3));
-    if ( sign1 * sign2 > 0 && sign1 * sign3 > 0) return false;//одинаковые знаки у всех расстояний
+    sign1 = triangle_handler::sign_of_dist(plane_1, tr2.getVertice(1));
+    sign2 = triangle_handler::sign_of_dist(plane_1, tr2.getVertice(2));
+    sign3 = triangle_handler::sign_of_dist(plane_1, tr2.getVertice(3));
+    if ( sign1 * sign2 > 0 && sign1 * sign3 > 0) return false;//треугольник полностью с одной стороны от плоскости
     //else continue
-    Plane plane_2 = cal_plane(tr2 );
-    if ( plane_1.get_norm_vec()._x/plane_2.get_norm_vec().x == plane_1.get_norm_vec()._y/plane_2.get_norm_vec()._y
-        && plane_1.get_norm_vec()._y/plane_2.get_norm_vec()._y == plane_1.get_norm_vec()._z/plane_2.get_norm_vec()._z) {
+    Plane plane_2 = Plane( tr2);
+    if ( triangle_handler::is_coincident( plane_1, plane_2, e)) {
         if (plane_1.get_d() == plane_2.get_d()) {
             /*
              * пересечение треугольников, лежащих в одной плоскости
              */
-        } else return false;//треугольники в параллельных плоскостях
+        }return false;//треугольники в параллельных плоскостях
     }
-
+    sign1 = triangle_handler::sign_of_dist(plane_2, tr1.getVertice(1));
+    sign2 = triangle_handler::sign_of_dist(plane_2, tr1.getVertice(2));
+    sign3 = triangle_handler::sign_of_dist(plane_2, tr1.getVertice(3));
+    if ( sign1 * sign2 > 0 && sign1 * sign3 > 0) return false;//треугольник полностью с одной стороны от плоскости
+    /*
+     * вычисление линии пересечения плоскостей
+     */
+    /*
+     * вычисление "проекций" на линию пересечения
+     */
+    /*
+     * если нет пересечений, то треуглоьники не пересекаются,
+     * иначе - пересекаются, и не надо дальше считать
+     */
 }
 
 int triangle_geometry::triangle_handler::sign_of_dist(const Plane &plane, const Point &point) const {
@@ -40,52 +50,79 @@ int triangle_geometry::triangle_handler::sign_of_dist(const Plane &plane, const 
     else return 0;
 }
 
-Plane triangle_geometry::triangle_handler::cal_plane(const triangle &tr) const {
-    return Plane(vec_mul(tr.get_vertice(1) - tr.get_vertice(2), tr.get_vertice(2) - tr.get_vertice(3)), tr.get_vertice(3));
+triangle_geometry::Plane::Plane() = default;
+
+triangle_geometry::Plane::Plane(const Point& norm_vec, const Point &point){
+    _norm_vec = norm_vec;
+    _point = point;
+    _d = triangle_handler::sc_pr( norm_vec, point);
 }
 
-triangle_geometry::Plane::Plane():Plane(0, 0) {}
+triangle_geometry::Plane::Plane(const triangle_geometry::triangle &tr): Plane( triangle_handler::vec_mul(tr.getVertice(1) - tr.getVertice(2),
+                                                                          tr.getVertice(2) - tr.getVertice(3)), tr.getVertice(3) ) {}
 
-triangle_geometry::Plane::Plane(const Point& norm_vec, &point):_norm_vec(norm_vec), _point(point),
-                                                                _d(triangle_handler::sc_pr(norm_vec, point)) {}
-
-Point triangle_geometry::Plane::get_norm_vec() {
+triangle_geometry::Point triangle_geometry::Plane::get_norm_vec() const{
     return _norm_vec;
 }
 
-double triangle_geometry::Plane::get_d() {
-    return d;
+double triangle_geometry::Plane::get_d() const{
+    return _d;
 }
 
-triangle_geometry::triangle::triangle(const Point &p1, int &p2, int &p3):(vertices[0])(p1),
-                                                                (vertices[1])(p2), (vertices[2])(p3) {}
-
-triangle_geometry::triangle::triangle():triangle(0.0, 0.0, 0.0){}
+triangle_geometry::triangle::triangle() = default;
 
 bool triangle_geometry::triangle::is_degenerate() const{//true if degenerate
-    Point norm_vec = triangle_handler::vec_mul(vertices[0] - vertices[1], vertices[1] - vertices[2] );
-    if ( norm_vec._x == 0 && norm_vec._y == 0 && norm_vec._z == 0) return true;
+    Point norm_vec = triangle_handler::vec_mul( _p1 - _p2, _p2 - _p3 );
+    if ( norm_vec.get_x() == 0 && norm_vec.get_y() == 0 && norm_vec.get_z() == 0) return true;
     return false;
 }
 
-triangle_geometry::Point::Point(const double &x, int &y, int &z):_x(x), _y(y), _z(z) {}
+triangle_geometry::Point::Point(const double &x, const double &y, const double &z): _x(x), _y(y), _z(z) {}
 
 triangle_geometry::Point::Point():Point(0.0, 0.0, 0.0)  {}
 
-triangle_geometry::Point::Point(const Point &source):_x(source._x), _y(source._y), _z(source._z) {}
+triangle_geometry::Point::Point(const Point &source) = default;
 
-Point triangle_geometry::Point::operator-(const Point &p2) const {
-    return Point(x - p2._x, y - p2._y, z - p2._z);
+triangle_geometry::Point triangle_geometry::Point::operator-(const Point &source) const {
+    return Point(_x - source._x, _y - source._y, _y - source._z);
 }
 
-Point triangle_geometry::triangle::get_vertice(const int &i) const {
-    return vertices[i-1];
+double triangle_geometry::Point::get_x() const {
+    return _x;
 }
 
-Point triangle_geometry::triangle_handler::vec_mul(const Point &p1, &p2) const {
-    return Point(p1._y * p2._z - p1._z * p2._y, p1._z * p2._x - p1._x * p2._z, p1._x * p2._y - p1._y * p2._x);
+double triangle_geometry::Point::get_y() const {
+    return _y;
 }
 
-double triangle_geometry::triangle_handler::sc_pr(const Point &p1, int &p2) const {
-    return ( p1._x * p2._x + p1._y * p2._y + p1._z * p2._z);
+double triangle_geometry::Point::get_z() const {
+    return _z;
+}
+
+triangle_geometry::Point triangle_geometry::triangle::getVertice(const int &i) const {
+    if ( i == 1) return _p1;
+    if ( i == 2) return _p2;
+    return _p3;
+}
+
+triangle_geometry::triangle::triangle(const triangle_geometry::Point &p1, const triangle_geometry::Point &p2,
+                                      const triangle_geometry::Point &p3): _p1(p1), _p2(p2), _p3(p3) {}
+
+triangle_geometry::Point triangle_geometry::triangle_handler::vec_mul(const Point &p1, const Point &p2){
+    return Point(p1.get_y() * p2.get_z() - p1.get_z() * p2.get_y(), p1.get_z() * p2.get_x() - p1.get_x() * p2.get_z(),
+                 p1.get_x() * p2.get_y() - p1.get_y() * p2.get_x());
+}
+
+double triangle_geometry::triangle_handler::sc_pr(const Point &p1, const Point &p2){
+    return ( p1.get_x() * p2.get_x() + p1.get_y() * p2.get_y() + p1.get_z() * p2.get_z());
+}
+
+bool triangle_geometry::triangle_handler::is_coincident(const triangle_geometry::Plane &p1,
+                                                        const triangle_geometry::Plane &p2, const double &e) const {
+    if ( (p1.get_norm_vec().get_x() - e) <= p2.get_norm_vec().get_x() && (p1.get_norm_vec().get_x() + e) >= p2.get_norm_vec().get_x()
+    && (p1.get_norm_vec().get_y() - e) <= p2.get_norm_vec().get_y() && (p1.get_norm_vec().get_y() + e) >= p2.get_norm_vec().get_y()
+    && (p1.get_norm_vec().get_z() - e) <= p2.get_norm_vec().get_z() && (p1.get_norm_vec().get_z() + e) >= p2.get_norm_vec().get_z()){
+        return true;
+    }
+    return false;
 }
