dd83230844ad5b99d0d0a96b712e75802ca2f681
hiotareq <62850531+hiotareq@users.noreply.github.com>
2020-10-16 00:58:00 +0300

    testing

--- triangles/main.cpp
+++ triangles/main.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
 #include "trianglespace.hpp"
+#include <fstream>
 
 int main(){
-    int NumberOfTriangles;
-    float MinCoord = 0, MaxCoord = 0;
-    std::vector<triangle_geometry::triangle> Triangles;
-    triangle_space::Cube* FirstCube = new triangle_space::Cube;
-    triangle_space::SpaceDivider* SpDiv = new triangle_space::SpaceDivider(8);
-    std::cout << "Введите количество треугольников" << std::endl;
-    std::cin >> NumberOfTriangles;
-    std::cout << "Введите вершины треугольников" << std::endl;
-    for ( int i = 0 ; i < NumberOfTriangles; i++){
-        triangle_geometry::triangle NewTriangle;
-        std::cin >> NewTriangle;
-        Triangles.push_back(NewTriangle);
-        if ( MinCoord > NewTriangle.get_min_coord()) MinCoord = NewTriangle.get_min_coord();
-        if ( MaxCoord < NewTriangle.get_max_coord()) MaxCoord = NewTriangle.get_max_coord();
-    }
-    FirstCube->SetCube(MinCoord, MinCoord, MinCoord, MaxCoord - MinCoord);
-    SpDiv->look_at_cube(*FirstCube);
-    std::cout << SpDiv->get_number_of_intersections() << std::endl;
+        std::cout << "started. Wait" << std::endl;
+        int NumberOfTriangles;
+        float MinCoord = 0, MaxCoord = 0;
+        std::vector<triangle_geometry::triangle> Triangles;
+        triangle_space::Cube* FirstCube = new triangle_space::Cube;
+        triangle_space::SpaceDivider* SpDiv = new triangle_space::SpaceDivider(8);
+        std::cout << "Введите количество треугольников" << std::endl;
+        std::cin >> NumberOfTriangles;
+        std::cout << "Введите вершины треугольников" << std::endl;
+        for ( int i = 0 ; i < NumberOfTriangles; i++){
+            triangle_geometry::triangle NewTriangle;
+            std::cin >> NewTriangle;
+            Triangles.push_back(NewTriangle);
+            if ( MinCoord > NewTriangle.get_min_coord()) MinCoord = NewTriangle.get_min_coord();
+            if ( MaxCoord < NewTriangle.get_max_coord()) MaxCoord = NewTriangle.get_max_coord();
+        }
+        FirstCube->SetCube(MinCoord, MinCoord, MinCoord, MaxCoord - MinCoord);
+        SpDiv->look_at_cube(*FirstCube);
+        std::cout << SpDiv->get_number_of_intersections() << std::endl;
     return 0;
 }
